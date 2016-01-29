caea5533ab4ab04f36c2ef11517bff558a25efbc
lyly22 <1184790972@qq.com>
2016-01-29 16:52:29 +0800

    touch

--- mobile/js/index.js
+++ mobile/js/index.js
@@ -2,7 +2,7 @@
 * @Author: Administrator
 * @Date:   2016-01-28 11:49:32
 * @Last Modified by:   Administrator
-* @Last Modified time: 2016-01-29 15:45:27
+* @Last Modified time: 2016-01-29 16:49:58
 */
 
 'use strict';
@@ -10,31 +10,63 @@ var winWidth=$('body').width();
 var liLength=$('.slider li').length;
 var slider=$('.slider');
 var sliderLi=$('.slider li');
-var timer,i=0;
+var timer,i=1;
 slider.width(liLength*winWidth);
 sliderLi.width(slider.width()/liLength);
 // $('.slider li').height($('.slider li').width()*0.4333);
 $('.banner').height(winWidth*0.38);
-// slider.css('left',-winWidth);
+slider.css('left',-winWidth);
+$('.circle li').eq(0).addClass('act').siblings().removeClass('act');
 function play(){
 	i++;
 	slider.animate({'left':-winWidth*i},800,function(){
-		$('.circle li').eq(i).addClass('act').siblings().removeClass('act');
+		$('.circle li').eq(i-1).addClass('act').siblings().removeClass('act');
 		if(i===liLength-1){
-			i=0;
+			i=1;
 			slider.animate({'left':-winWidth*i},0);			
-			$('.circle li').eq(i).addClass('act').siblings().removeClass('act');
-		}		
-		
+			$('.circle li').eq(i-1).addClass('act').siblings().removeClass('act');
+		}	
 	});
-	
-	// slider.css('webkitTransform','translateX(-'+winWidth*i+')');
-	// slider.css('webkitTransition','1s');
+};
+
+function touchRight(){
+	i--;
+	slider.animate({'left':-winWidth*i},800,function(){
+		$('.circle li').eq(i-1).addClass('act').siblings().removeClass('act');
+		if(i===0){
+			i=liLength-2;
+			slider.animate({'left':-winWidth*i},0);			
+			$('.circle li').eq(i-1).addClass('act').siblings().removeClass('act');
+		}	
+	});
 }
 timer=setInterval('play()',3000);
+
+var banner=$('.banner')[0];
+var startx,endx;
+banner.addEventListener('touchstart',function(e){
+	startx=e.touches[0].pageX;
+});
+banner.addEventListener('touchmove',function(e){
+	e.preventDefault();
+});
+banner.addEventListener('touchend',function(e){
+		clearInterval(timer);
+		endx=e.changedTouches[0].pageX;
+		var deltaX=endx-startx;
+		if(deltaX>0){
+			//right
+			touchRight();
+			window.setTimeout(function(){timer=setInterval('play()',3000);},8000);
+		}else if(deltaX<0){
+			play();
+			window.setTimeout(function(){timer=setInterval('play()',3000);},8000);
+		}		
+});
 
 $('.question-list span').click(function(){
 	$(this).siblings('.answer').slideToggle();
 });
 
 $('#about').addClass('current');
+
